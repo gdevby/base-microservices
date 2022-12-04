c253b28e320a2f73564704ebefa86f151a0961bd
robert <robert@work-1>
2022-12-04 16:12:27 +0300

    added authenticate and authorization for gateway

--- gateway/src/main/java/by/gdev/gateway/RoutesApplication.java
+++ gateway/src/main/java/by/gdev/gateway/RoutesApplication.java
@@ -2,10 +2,13 @@ package by.gdev.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 @EnableEurekaClient
+@RefreshScope
 public class RoutesApplication {
 
 	public static void main(String[] args) {
